f726ca2c5c63935151a00e41143ea7018d629f69
Tien Nguyen <tien.nguyenvan@grabtaxi.com>
2020-01-04 13:22:42 +0700

    [Restless] Completed interface of record service

--- record/transport/grpc.go
+++ record/transport/grpc.go
@@ -23,6 +23,18 @@ func (g *grpcServer) ListRecords(context.Context, *pb.ListRecordsRequest) (*pb.L
 	return nil, errors.New("please implement me")
 }
 
+func (g *grpcServer) RetrieveRecord(ctx context.Context, req *pb.RetrievesRequest) (*pb.RecordMsg, error) {
+	return nil, errors.New("please implement me")
+}
+
+func (g *grpcServer) UpdateRecords(ctx context.Context, req *pb.CreateRecordsRequest) (*pb.ListRecordsReply, error) {
+	return nil, errors.New("please implement me")
+}
+
 func (g *grpcServer) CreateRecords(ctx context.Context, req *pb.CreateRecordsRequest) (*pb.ListRecordsReply, error) {
+	return nil, errors.New("please implement me")
+}
+
+func (g *grpcServer) DeleteRecords(ctx context.Context, req *pb.DeleteRecordsRequest) (*pb.DeleteRecordsReply, error) {
 	return nil, errors.New("please implement me")
 }
