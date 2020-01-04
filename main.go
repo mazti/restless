e6954ce2cf38fb724e09aae93aee993d8210af1f
Tien Nguyen <tien.nguyenvan@grabtaxi.com>
2020-01-04 13:31:57 +0700

    [Restless] Add command to cobra

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	base "github.com/mazti/restless/base/cmd"
 	meta "github.com/mazti/restless/meta/cmd"
+	record "github.com/mazti/restless/record/cmd"
 	"github.com/spf13/cobra"
 )
 
@@ -17,5 +18,6 @@ var (
 func main() {
 	RestlessCmd.AddCommand(base.BaseCmd)
 	RestlessCmd.AddCommand(meta.MetaCmd)
+	RestlessCmd.AddCommand(record.RecordCmd)
 	RestlessCmd.Execute()
 }
